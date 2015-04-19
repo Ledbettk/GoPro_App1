25e5a6172e199f05e05fc95ed3bc74731c3ba39d
sruppgreene <siru4581@colorado.edu>
2015-04-19 16:53:56 -0600

    changed some landing page stuff, added route i think. i forgot what i changed since last time i worked on it sorrryy

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,21 @@ class VideosController < ApplicationController
     def new
         @video = video.new
     end
+
+    def create
+    	@video = video.new(video_params)
+    	if @video.save
+    		index
+    	else
+    		puts "something went wrong in creating video"
+    end
+
+    def index
+    	@videos = Video.all
+    end
+
+    def show_video
+    	@video = Videos.find(params[:id])
+    	render video_page
+    end
 end
